017a03906b6d09f3a3647276619cceb936be805b
darrenc777 <darrenc77@nycstudents.net>
2025-11-20 11:35:26 -0500

    AppointmentBook

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,17 +1,24 @@
-public class AppointmentBook {
+public class AppointmentBook
+{
     private boolean[][] schedule;
-    public AppointmentBook(boolean[][] schedule) {
+    public AppointmentBook(boolean[][] schedule)
+    {
         this.schedule = schedule;
     }
-    private boolean isMinuteFree(int period, int minute) {
+    private boolean isMinuteFree(int period, int minute)
+    {
         return schedule[period - 1][minute];
     }
-    public int findFreeBlock(int period, int duration){
+    public int findFreeBlock(int period, int duration)
+    {
         int block = 0;
-        for(int i = 0; i < 60; i++){
-            if(isMinuteFree(period, i)){
+        for(int i = 0; i < 60; i++)
+        {
+            if(isMinuteFree(period, i))
+            {
                 block++;
-                if(block == duration){
+                if(block == duration)
+                {
                     return i - duration + 1;
                 }
             }
@@ -19,23 +26,29 @@ public class AppointmentBook {
         }
         return -1;
     }
-    private void reserveBlock(int period, int startMinute, int duration){
+    private void reserveBlock(int period, int startMinute, int duration)
+    {
 
-        for(int i = startMinute; i < startMinute + duration; i++){
+        for(int i = startMinute; i < startMinute + duration; i++)
+        {
             schedule[period -1][i] = false;
         }
     }
-    public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
-        for(int i = startPeriod; i <= endPeriod; i++){
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
+    {
+        for(int i = startPeriod; i <= endPeriod; i++)
+        {
             int freeBlock = findFreeBlock(i, duration);
-            if (freeBlock > -1){
+            if (freeBlock > -1)
+            {
                 reserveBlock(i, freeBlock, duration);
                 return true;
             }
         }
         return false;
     }
-    public void printPeriod(int period){
+    public void printPeriod(int period)
+    {
         for(int i = 0; i < schedule[period - 1].length; i++)
             System.out.println(i + " " + schedule[period - 1][i]);
     }
