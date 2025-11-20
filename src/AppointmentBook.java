9d06ddeddbed9dae2496a3119f7565f603e28195
darrenc777 <darrenc77@nycstudents.net>
2025-11-20 11:12:01 -0500

    AppointmentBook

---
+++ src/AppointmentBook.java
@@ -0,0 +1,16 @@
+public class AppointmentBook
+{
+    private boolean[][] schedule;
+    public AppointmentBook(boolean[][] schedule)
+    {
+        this.schedule = schedule;
+    }
+    private boolean isMinuteFree(int period, int minute) {
+        return schedule[period - 1][minute];
+    }
+    public int findFreeBlock(int period, int duration) {
+        return 0;
+    }
+    public boolean makeAppointment(int startPeriod, int endPeriod,)
+        return false;
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    boolean[][] schedule = new boolean [8][60];
+    AppointmentBook a = new AppointmentBook(schedule);
+    for(int i = 10; i < 15; i++)
+    {
+        schedule[1][1] = true
+        a.printPeriod(2);
+    }
+    public void printPeriod(int period)
+    {
+        for9int i = 0; i < schedule[period - 1].length; i++)
+        System.out.println((i + " " + schedule[period - 1][1]);
+    }
+    public int findFreeBlock(int period, int duration) {
+        int block = 0;
+        for(int i = 0; i < 60; i++)
+            if(isMinuteFree)
+    }
+}
